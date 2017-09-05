185f1f544f30af43b0a01a1ed56f8df0475b2b02
Otso Jousimaa <otso@ojousima.net>
2017-09-05 23:25:01 +0300

    Update ruuvi.webbluetooth.js

--- js/bundle.js
+++ js/bundle.js
@@ -10542,7 +10542,7 @@ class serviceInterface{
         let characteristic = this.getCharacteristicByUUID(characteristics[ii]);
         //Continue if characteristic is not known in service
         if(!characteristic) {continue;}
-        characteristic.handle = await this.serviceHandle.getCharacteristic(this.TX.UUID);
+        characteristic.handle = await this.serviceHandle.getCharacteristic(characteristic.UUID);
         characteristic.onChange = function(event) 
         {
           if(this.callback){
